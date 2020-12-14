61c04401d246a26460b743b0fd0174d0f2487b12
guosijia <guosijia@sjtu.edu.cn>
2020-12-14 19:12:00 +0800

    后端Flask RESTful API 提供测试接口Ping-Pong

---
+++ back-end/psymukb.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/14 18:36
+# @Author  : Sijia
+# @Email   : guosijia007@yeah.net
+# @File    : psymukb.py
+# @Software: PyCharm
+
+from app import create_app
+
+app = create_app()
